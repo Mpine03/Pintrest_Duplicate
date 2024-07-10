f41d0cce437aefad5c27b7f56524ca26fd361802
Nthabeleng Khabele <104368079+Mpine03@users.noreply.github.com>
2024-07-11 01:09:45 +0200

    Add files via upload

---
+++ Pintrest_Clone/script.js
@@ -0,0 +1,249 @@
+const items = [
+    
+    {
+        id: "item-1",
+        imageUrl: "https://i.pinimg.com/originals/a5/af/de/a5afdec28dbdc8f52584b78eb4ceaf87.jpg",
+        imageProfileImage: "https://i.pinimg.com/280x280_RS/a3/85/a7/a385a7a86b986477b477b4d76a0f4bad.jpg",
+        profileName: "ainursyahirah",
+        suggestedCategory: "food",
+        title: "Donuts"
+    },
+    
+    {
+        id: "item-2",
+        imageUrl: "https://i.pinimg.com/originals/79/73/42/797342f744963dad9595a8163602f692.jpg",
+        imageProfileImage: "https://i.pinimg.com/280x280_RS/2f/94/3d/2f943da87232950f70718b8cb76a8cc9.jpg",
+        profileName: "adygrace4903",
+        suggestedCategory: "christian",
+        title: "Christian Wallpapers"
+    },
+    {
+        id: "item-3",
+        imageUrl: "https://i.pinimg.com/originals/04/82/05/04820525e0bab37a5faaaa1efd6059ea.jpg",
+        imageProfileImage: "https://i.pinimg.com/280x280_RS/cd/4a/23/cd4a23fad89dbca54054d0845237e0c0.jpg",
+        profileName: "frombrenda",
+        suggestedCategory: "laptop",
+        title: "Computer Science aesthetic"
+    },
+    {
+        id: "item-4",
+        imageUrl: "https://i.pinimg.com/originals/7a/87/ca/7a87caf27f3fd3e3ec179942eb051f78.jpg",
+        imageProfileImage: "https://i.pinimg.com/280x280_RS/a4/77/95/a47795d336ccbc44a2c2648fd2ce8037.jpg",
+        profileName: "Qxuiraa",
+        suggestedCategory: "beauty",
+        title: "Nails"
+    },
+    {
+        id: "item-5",
+        imageUrl: "https://i.pinimg.com/originals/d4/89/b6/d489b609619981c75414a965322511a0.jpg",
+        imageProfileImage: "https://i.pinimg.com/280x280_RS/46/ca/03/46ca03733f164f8a048931ead7b46751.jpg",
+        profileName: "wubbolovesmaggi",
+        suggestedCategory: "jewellary",
+        title: "Piercings"
+    },
+    {
+        id: "item-6",
+        imageUrl: "https://i.pinimg.com/originals/bc/cf/ad/bccfad3c1616f5788f5b659dc4815353.jpg",
+        imageProfileImage: "https://i.pinimg.com/280x280_RS/09/05/4b/09054b7d53904177b36d5b397281e82b.jpg",
+        profileName: "wonniekati",
+        suggestedCategory: "iphone",
+        title: "Apple devices"
+    },
+    {
+        id: "item-7",
+        imageUrl: "https://i.pinimg.com/originals/92/60/a5/9260a5f97f1cfb597a03ed981c2ad688.jpg",
+        imageProfileImage: "https://i.pinimg.com/280x280_RS/d3/2c/80/d32c8016748ac7ec09fe88ea27a3a0be.jpg",
+        profileName: "nimsey",
+        suggestedCategory: "programmer",
+        title: "Programmer jokes"
+    },
+    {
+        id: "item-8",
+        imageUrl: "https://i.pinimg.com/originals/55/af/3e/55af3e2501390f44b47c54a86207a7f6.jpg",
+        imageProfileImage: "https://i.pinimg.com/280x280_RS/d7/b6/06/d7b6066bead850e7614f222eed9e3a6c.jpg",
+        profileName: "suesylvesterstracksuit",
+        suggestedCategory: "fashion",
+        title: "Fashionable Look"
+    },
+    {
+        id: "item-9",
+        imageUrl: "https://i.pinimg.com/originals/c8/28/ce/c828ce5d1760c2682337819d7610fef8.jpg",
+        imageProfileImage: "https://i.pinimg.com/280x280_RS/9c/40/8f/9c408f6e2dacdb60beef215f8f0f274c.jpg",
+        profileName: "zainabjafri11025",
+        suggestedCategory: "desert",
+        title: "Ice Cream"
+    },
+    {
+        id: "item-10",
+        imageUrl: "https://i.pinimg.com/originals/0e/b5/d4/0eb5d40de268f48c828dd5c00700ea80.jpg",
+        imageProfileImage: "https://i.pinimg.com/236x/ec/ee/6b/ecee6b06df14385e538b5c8a8cc5c599.jpg",
+        profileName: "manuel",
+        suggestedCategory: "fashion",
+        title: "Fashionable Look"
+    },
+    {
+        id: "item-11",
+        imageUrl: "https://i.pinimg.com/474x/af/3c/7e/af3c7e32ca9b4776af56a5e0bf84df1a.jpg",
+        imageProfileImage: "https://i.pinimg.com/280x280_RS/cc/26/fe/cc26fe0541dcb0266396a8df5d55066d.jpg",
+        profileName: "moonpie_18",
+        suggestedCategory: "christian",
+        title: "Christian Wallpapers"
+    },
+    {
+        id: "item-12",
+        imageUrl: "https://i.pinimg.com/236x/3a/0a/fd/3a0afddedc223a3591b3a682974b4c64.jpg",
+        imageProfileImage: "https://i.pinimg.com/280x280_RS/d4/e6/7b/d4e67b5e39d58eff91afeac6f1c9904e.jpg",
+        profileName: "sniffedawhitestripe",
+        suggestedCategory: "roses",
+        title: "Roses"
+    },
+    {
+        id: "item-13",
+        imageUrl: "https://i.pinimg.com/236x/3d/59/e9/3d59e94fdaf4abbdd7f1c4035281069f.jpg",
+        imageProfileImage: "https://i.pinimg.com/280x280_RS/a8/3b/a3/a83ba361b3d92152aed53ba6880212f4.jpg",
+        profileName: "dcoleman2062",
+        suggestedCategory: "cars",
+        title: "Pink G-Wagon"
+    },
+    {
+        id: "item-14",
+        imageUrl: "https://i.pinimg.com/236x/53/e0/06/53e0060813b937ca4296cb1689a06b5a.jpg",
+        imageProfileImage: "https://i.pinimg.com/75x75_RS/b5/d5/88/b5d58854237e0323eaf46a63252fbd09.jpg",
+        profileName: "berrybell7",
+        suggestedCategory: "spongebob",
+        title: "Spongebob Wallpapers"
+    },
+    {
+        id: "item-15",
+        imageUrl: "https://i.pinimg.com/474x/39/eb/ec/39ebec56a1b66378629c51ebb16a2849.jpg",
+        imageProfileImage: "https://i.pinimg.com/280x280_RS/2d/2f/40/2d2f4081412991693ef2b1a8bc3ef8db.jpg",
+        profileName: "truubeautys",
+        suggestedCategory: "cake",
+        title: "Graduation Cake"
+    },
+    {
+        id: "item-16",
+        imageUrl: "https://i.pinimg.com/originals/59/1a/59/591a598f3a6825f463b9844cc5a2fcfa.jpg",
+        imageProfileImage: "https://i.pinimg.com/280x280_RS/92/da/8e/92da8ec2875e568a353ea2387cd22b54.jpg",
+        profileName: "dark47433",
+        suggestedCategory: "coffee mobile app",
+        title: "Coffee mobile app UI"
+    },
+    {
+        id: "item-17",
+        imageUrl: "https://i.pinimg.com/originals/cc/0a/9a/cc0a9a6b79f75eb52ea671d1c6842bcf.jpg",
+        imageProfileImage: "https://i.pinimg.com/280x280_RS/a7/a5/9e/a7a59ec577ce7303c0480a91aa583a1f.jpg",
+        profileName: "xixi0828",
+        suggestedCategory: "room decor",
+        title: "Room Decor"
+    },
+    {
+        id: "item-18",
+        imageUrl: "https://i.pinimg.com/originals/cc/f1/a4/ccf1a426fa4cc67133cff1179de12760.jpg",
+        imageProfileImage: "https://i.pinimg.com/280x280_RS/70/44/4a/70444acf046046babf44322ac243c52a.jpg",
+        profileName: "rlmckernan11",
+        suggestedCategory: "cartoons",
+        title: "Cartoon pig"
+    },
+    {
+        id: "item-19",
+        imageUrl: "https://i.pinimg.com/originals/9d/2d/50/9d2d502c75286f8c487d497b01c450db.jpg",
+        imageProfileImage: "https://i.pinimg.com/280x280_RS/da/f1/0c/daf10c4f63189d66b8ef23f51633ab59.jpg",
+        profileName: "mackenziiee_",
+        suggestedCategory: "food",
+        title: "Food"
+    },
+    {
+        id: "item-20",
+        imageUrl: "https://i.pinimg.com/originals/14/bb/16/14bb16b072759e6e19847c0890d1c9c4.jpg",
+        imageProfileImage: "https://i.pinimg.com/280x280_RS/09/e9/e0/09e9e036d715c265e55e9c98a5b70455.jpg",
+        profileName: "castle101chocolate",
+        suggestedCategory: "Sweets",
+        title: "Gummies"
+    },
+      
+   
+];
+
+const mainContent = document.getElementById('main-content');
+const searchBar = document.getElementById('search-bar');
+
+function displayItems(items) {
+    mainContent.innerHTML = '';
+    items.forEach(item => {
+        const itemElement = document.createElement('div');
+        itemElement.classList.add('item');
+        itemElement.innerHTML = `
+            <div class="image-container">
+                <img src="${item.imageUrl}" alt="${item.title}">
+                <div class="hover-content">
+                    <div class="category">
+                        ${item.suggestedCategory}
+                        <i class="fas fa-chevron-down "></i>
+                    </div>
+                    <div class="save-button">Save</div>
+                    <div class="bottom-right-icons">
+                        <i class="fas fa-download icon-button"></i>
+                        <i class="fas fa-ellipsis-h icon-button"></i>
+                    </div>
+                </div>
+            </div>
+            <div class="title">${item.title}</div>
+            <div class="profile">
+                <img src="${item.imageProfileImage}" alt="${item.profileName}">
+                <span class="profile-name">${item.profileName}</span>
+            </div>
+        `;
+        mainContent.appendChild(itemElement);
+    });
+
+    
+    masonryLayout();
+}
+
+function filterItems(event) {
+    const query = event.target.value.toLowerCase();
+    const filteredItems = items.filter(item => 
+        item.title.toLowerCase().includes(query) || 
+        item.profileName.toLowerCase().includes(query)
+    );
+    displayItems(filteredItems);
+}
+
+
+displayItems(items);
+
+
+searchBar.addEventListener('input', filterItems);
+
+
+function masonryLayout() {
+    const columns = 5; 
+    const containerWidth = mainContent.clientWidth;
+    const gutter = 20;
+    const itemWidth = (containerWidth / columns) - gutter;
+    const itemElements = mainContent.querySelectorAll('.item');
+    let columnHeights = Array.from({ length: columns }, () => 0);
+
+    itemElements.forEach(item => {
+        const minHeight = Math.min(...columnHeights);
+        const columnIndex = columnHeights.indexOf(minHeight);
+        const leftOffset = columnIndex * (itemWidth + gutter);
+        const topOffset = columnHeights[columnIndex];
+
+        item.style.position = 'absolute';
+        item.style.width = `${itemWidth}px`; 
+        item.style.top = `${topOffset}px`;
+        item.style.left = `${leftOffset}px`;
+
+        columnHeights[columnIndex] += item.offsetHeight + gutter; 
+        mainContent.style.height = `${Math.max(...columnHeights)}px`; 
+    });
+}
+
+
+window.addEventListener('load', () => {
+    masonryLayout();
+});
+
+window.addEventListener('resize', masonryLayout);
